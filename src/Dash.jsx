487505cf0077c28cac6ddb405efc8b9402018e97
Mathew Peachey <mathew.peachey@live.com>
2021-11-06 22:51:17 +1300

    working filters

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -1,36 +1,18 @@
 import DisplayData from "./DisplayData";
 import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-
-const initialDashletData = [
-	{
-		indicator: 1,
-		name: "1",
-	},
-	{
-		indicator: 2,
-		name: "2",
-	},
-	{
-		indicator: 3,
-		name: "3",
-	},
-];
 
 const Dash = () => {
-	const [dashlets, setDashlets] = useState(initialDashletData);
-
 	return (
 		<Grid container spacing={2}>
 			<Grid item xs={12} md={6}>
-				<DisplayData indicator={"1"} />
+				<DisplayData selectedIndicator={1} selectedSeries={1} selectedSubseries={1} />
 			</Grid>
 			<Grid item xs={12} md={6}>
-				<DisplayData indicator={"2"} />
+				<DisplayData selectedIndicator={2} selectedSeries={3} selectedSubseries={5} />
 			</Grid>
 			<Grid item xs={12} md={6}>
-				<DisplayData indicator={"3"} />
+				<DisplayData selectedIndicator={2} selectedSeries={3} selectedSubseries={4} />
 			</Grid>
 		</Grid>
 	);
--- src/DisplayData.jsx
+++ src/DisplayData.jsx
@@ -9,18 +9,12 @@ import Select from "@mui/material/Select";
 
 import "./Dashlet.css";
 
-const eventsByIndicator = gql`
-	query getEventsByIndicator($indicatorId: Int!) {
-		indicatorById(id: $indicatorId) {
-			eventsByIndicatorId {
-				nodes {
-					id
-					indicatorId
-					seriesId
-					subSeriesId
-					date
-					value
-				}
+const eventsQuery = gql`
+	query getEvents($indicatorId: Int!, $seriesId: Int!, $subSeriesId: Int!) {
+		allEvents(condition: { indicatorId: $indicatorId, seriesId: $seriesId, subSeriesId: $subSeriesId }) {
+			nodes {
+				date
+				value
 			}
 		}
 	}
@@ -37,23 +31,49 @@ const allIndicators = gql`
 	}
 `;
 
-const DisplayData = ({ indicator }) => {
-	const [activeIndicator, setIndicator] = useState(indicator);
+const allSeries = gql`
+	query {
+		allSeries {
+			nodes {
+				id
+				name
+			}
+		}
+	}
+`;
+
+const allSubSeries = gql`
+	query {
+		allSubSeries {
+			nodes {
+				id
+				name
+			}
+		}
+	}
+`;
+
+const DisplayData = ({ selectedIndicator, selectedSeries, selectedSubseries }) => {
+	const [activeIndicator, setIndicator] = useState(selectedIndicator);
+	const [activeSeries, setSeries] = useState(selectedSeries);
+	const [activeSubSeries, setSubSeries] = useState(selectedSubseries);
 
 	const indicators = useQuery(allIndicators);
-	const { data } = useQuery(eventsByIndicator, {
-		variables: { indicatorId: activeIndicator },
+	const series = useQuery(allSeries);
+	const subseries = useQuery(allSubSeries);
+
+	const { data } = useQuery(eventsQuery, {
+		variables: { indicatorId: activeIndicator, seriesId: activeSeries, subSeriesId: activeSubSeries },
 	});
 
 	return (
 		<Card sx={{ margin: 2 }} variant="outlined">
 			<CardContent>
-				<InputLabel id="demo-simple-select-label">Indicator</InputLabel>
+				<InputLabel id="indicator-select-label">Indicator</InputLabel>
 				<Select
-					labelId="demo-simple-select-label"
-					id="demo-simple-select"
+					labelId="indicator-select-label"
 					label="Indicator"
-					value={indicator}
+					value={activeIndicator}
 					onChange={(e) => {
 						setIndicator(parseInt(e.target.value));
 					}}
@@ -64,8 +84,41 @@ const DisplayData = ({ indicator }) => {
 						</MenuItem>
 					))}
 				</Select>
+
+				<InputLabel id="series-select-label">Series</InputLabel>
+				<Select
+					labelId="series-select-label"
+					label="Series"
+					value={activeSeries}
+					onChange={(e) => {
+						setSeries(parseInt(e.target.value));
+					}}
+				>
+					{series?.data?.allSeries?.nodes.map((i) => (
+						<MenuItem key={i.id} value={i.id}>
+							{i.name}
+						</MenuItem>
+					))}
+				</Select>
+
+				<InputLabel id="sub-series-select-label">Sub-Series</InputLabel>
+				<Select
+					labelId="sub-series-select-label"
+					label="Sub-Series"
+					value={activeSubSeries}
+					onChange={(e) => {
+						setSubSeries(parseInt(e.target.value));
+					}}
+				>
+					{subseries?.data?.allSubSeries?.nodes.map((i) => (
+						<MenuItem key={i.id} value={i.id}>
+							{i.name}
+						</MenuItem>
+					))}
+				</Select>
+
 				<ResponsiveContainer width="95%" height={400}>
-					<LineChart width={600} height={400} data={data?.indicatorById?.eventsByIndicatorId?.nodes}>
+					<LineChart width={600} height={400} data={data?.allEvents?.nodes}>
 						<Line type="monotone" dataKey="value" stroke="#8884d8" />
 						<XAxis dataKey="name" />
 						<YAxis />
